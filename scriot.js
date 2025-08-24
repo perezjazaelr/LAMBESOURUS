b74129c0291ff413ce4ff867aebb5ee55406c2ef
Jazael Perez <131943183+perezjazaelr@users.noreply.github.com>
2025-08-24 18:15:51 +0800

    Add files via upload

---
+++ scriot.js
@@ -0,0 +1,659 @@
+/* MAIN */
+function home(){
+    location.href=("index.html");
+}
+
+function subtopic(){
+    location.href=("sb-introduction.html");
+}
+function guide(){
+    location.href=("userGuide.html");
+}
+
+
+/* LANDING PAGE */
+function introduction(){
+    location.href=("sb-introduction.html");
+}
+function operator(){
+    location.href=("sb-operators.html");
+}
+function crtlState(){
+    location.href=("sb-CrtlState.html");
+}
+function Arrays(){
+    location.href=("sb-arrays.html");
+}
+function functions(){
+    location.href=("sb-functions.html");
+}
+function superGlobal(){
+    location.href=("sb-superGlobal.html");
+}
+function webDev(){
+    location.href=("sb-web.html");
+}
+function fileHandling(){
+    location.href=("sb-fileHandling.html");
+}
+function ajax(){
+    location.href=("sb-ajax.html");
+}
+function advance(){
+    location.href=("sb-advance.html");
+}
+function useful(){
+    location.href=("sb-useful.html");
+}
+function login(){
+    location.href=("sb-login.html");
+}
+function xml(){
+    location.href=("sb-xml.html");
+}
+function objectOriented(){
+    location.href=("sb-oriented.html");
+}
+
+
+
+
+/* INTRODUCTION*/
+function hello(){
+    location.href=("intro/intro-hello.html");
+}
+function echo(){
+    location.href=("intro/intro-echo.html");
+}
+function echo(){
+    location.href=("intro/intro-echo.html");
+}
+function constant(){
+    location.href=("intro/intro-constant.html");
+}
+function typecasting(){
+    location.href=("intro/intro-typeCasting.html");
+}
+function boolean(){
+    location.href=("intro/intro-boolean.html");
+}
+function math(){
+    location.href=("intro/intro-math.html");
+}
+function file(){
+    location.href=("intro/intro-file.html");
+}
+function comment(){
+    location.href=("intro/intro-comment.html");
+}
+function vardump(){
+    location.href=("intro/intro-vardump.html");
+}
+function magicconstant(){
+    location.href=("intro/intro-magicconstant.html");
+}
+function typejuggling(){
+    location.href=("intro/intro-typejug.html");
+}
+function integer(){
+    location.href=("intro/intro-integer.html");
+}
+function heredoc(){
+    location.href=("intro/intro-heredoc.html");
+}
+function date(){
+    location.href=("intro/intro-date.html");
+}
+function variable(){
+    location.href=("intro/intro-variables.html");
+}
+function $and(){
+    location.href=("intro/intro-$and&&.html");
+}
+function datatype(){
+    location.href=("intro/intro-datatype.html");
+}
+function string(){
+    location.href=("intro/intro-string.html");
+}
+function io(){
+    location.href=("intro/intro-io.html");
+}
+function compound(){
+    location.href=("intro/intro-compound.html");
+}
+function scalar(){
+    location.href=("intro/intro-scalar.html");
+}
+function declaration(){
+    location.href=("intro/intro-declaration.html");
+}
+
+/* OPERATORS */
+function Arithmatic(){
+    location.href=("operators/oper-Arithmatic.html");
+}
+function Comparison(){
+    location.href=("operators/oper-Comparison.html");
+}
+function Logical(){
+    location.href=("operators/oper-Logical.html");
+}
+function Assignment(){
+    location.href=("operators/oper-Assignment.html");
+}
+function Stringoper(){
+    location.href=("operators/oper-Stringoper.html");
+}
+function Arrayoper(){
+    location.href=("operators/oper-Arrayoper.html");
+}
+function Conditional(){
+    location.href=("operators/oper-Conditional.html");
+}
+function Spread(){
+    location.href=("operators/oper-Spread.html");
+}
+function Null(){
+    location.href=("operators/oper-Null.html");
+}
+function Spaceship(){
+    location.href=("operators/oper-Spaceship.html");
+}
+
+/* CONTROL STATEMENT */
+function DecisionMaking(){
+    location.href=("control/control-DecisionMaking.html");
+}
+function WhileLoop(){
+    location.href=("control/control-WhileLoop.html");
+}
+function IfElse(){
+    location.href=("control/control-IfElse.html");
+}
+function ForLoop(){
+    location.href=("control/control-ForLoop.html");
+}
+function DoWhile(){
+    location.href=("control/control-DoWhile.html");
+}
+function ContinueState(){
+    location.href=("control/control-ContinueState.html");
+}
+function SwitchState(){
+    location.href=("control/control-SwitchState.html");
+}
+function ForeachLoop(){
+    location.href=("control/control-ForeachLoop.html");
+}
+function BreakState(){
+    location.href=("control/control-BreakState.html");
+}
+
+
+/* ARRAYS */
+function Indexed(){
+    location.href=("arrays/array-Indexed.html");
+}
+function Associative(){
+    location.href=("arrays/array-Associative.html");
+}
+function Multidimensional(){
+    location.href=("arrays/array-Multidimensional.html");
+}
+function Functions(){
+    location.href=("arrays/array-Functions.html");
+}
+function ConstantArr(){
+    location.href=("arrays/array-ConstantArr.html");
+}
+
+
+/* FUNCTION */
+function FunctionParameter(){
+    location.href=("functions/function-FunctionParameter.html");
+}
+function Callbyvalue(){
+    location.href=("functions/function-Callbyvalue.html");
+}
+
+function CallbyReference(){
+    location.href=("functions/function-CallbyReference.html");
+}
+
+function DefaultArguments(){
+    location.href=("functions/function-DefaultArguments.html");
+}
+
+function NamedArguments(){
+    location.href=("functions/function-NamedArguments.html");
+}
+
+function VariableArguments(){
+    location.href=("functions/function-VariableArguments.html");
+}
+
+function ReturningValues (){
+    location.href=("functions/function-ReturningValues.html");
+}
+
+function PassingFunctions(){
+    location.href=("functions/function-PassingFunctions.html");
+}
+
+function RecursiveFunctions(){
+    location.href=("functions/function-RecursiveFunctions.html");
+}
+
+function TypeHints(){
+    location.href=("functions/function-TypeHints.html");
+}
+
+function  VariableScope(){
+    location.href=("functions/function-VariableScope.html");
+}
+
+function  StrictTyping(){
+    location.href=("functions/function-StrictTyping.html");
+}
+
+function AnonymousFunctions(){
+    location.href=("functions/function-AnonymousFunctions.html");
+}
+
+function ArrowFunctions(){
+    location.href=("functions/function-ArrowFunctions.html");
+}
+
+function VariableFunctions(){
+    location.href=("functions/function-VariableFunctions.html");
+}
+
+function LocalVariables(){
+    location.href=("functions/function-LocalVariables.html");
+}
+function  GlobalVariables(){
+    location.href=("functions/function-GlobalVariables.html");
+}
+
+
+
+
+/*SUPER GLOBAL */
+function $GLOBALS(){
+    location.href=("superglobals/global-$GLOBALS.html");
+}
+function $_SERVER(){
+    location.href=("superglobals/global-$_SERVER.html");
+}
+function $_REQUEST(){
+    location.href=("superglobals/global-$_REQUEST.html");
+}
+function $_POST(){
+    location.href=("superglobals/global-$_POST.html");
+}
+function $_GET(){
+    location.href=("superglobals/global-$_GET.html");
+}
+function $_FILES(){
+    location.href=("superglobals/global-$_FILES.html");
+}
+function $_ENV(){
+    location.href=("superglobals/global-$_ENV.html");
+}
+function $_COOKIE(){
+    location.href=("superglobals/global-$_COOKIE.html");
+}
+function $_SESSION(){
+    location.href=("superglobals/global-$_SESSION.html");
+}
+
+
+
+/* FILE HANDLING */
+
+function OpenFile(){
+    location.href=("file-handling/handling-OpenFile.html");
+}
+function ReadFile(){
+    location.href=("file-handling/handling-ReadFile.html");
+}
+
+function WriteFile(){
+    location.href=("file-handling/handling-WriteFile.html");
+}
+
+function FileExistence(){
+    location.href=("file-handling/handlingFileExistence.html");
+}
+
+function DownloadFile(){
+    location.href=("file-handling/handling-DownloadFile.html");
+}
+
+function CopyFile(){
+    location.href=("file-handling/handling-CopyFile.html");
+}
+
+function AppendFile(){
+    location.href=("file-handling/handling-AppendFile.html");
+}
+
+function DeleteFile(){
+    location.href=("file-handling/handling-DeleteFile.html");
+}
+
+function HandleCSVFile(){
+    location.href=("file-handling/handling-HandleCSVFile.html");
+}
+
+function FilePermissions(){
+    location.href=("file-handling/handling-FilePermissions.html");
+}
+function CreateDirectory(){
+    location.href=("file-handling/handling-CreateDirectory.html");
+}
+function ListingFiles(){
+    location.href=("file-handling/handling-ListingFiles.html");
+}
+
+
+/* ORIENTED */
+function ClassesandObjects(){
+    location.href=("oop/oriented-ClassesandObjects.html");
+}
+function ConstructorandDestructor(){
+    location.href=("oop/oriented-ConstructorandDestructor.html");
+}
+function AccessModifiers(){
+    location.href=("oop/oriented-AccessModifiers.html");
+}
+function Inheritance(){
+    location.href=("oop/oriented-Inheritance.html");
+}
+function ClassConstants(){
+    location.href=("oop/oriented-ClassConstants.html");
+}
+function AbstractClasses(){
+    location.href=("oop/oriented-AbstractClasses.html");
+}
+function Interfaces(){
+    location.href=("oop/oriented-Interfaces.html");
+}
+function Traits(){
+    location.href=("oop/oriented-Traits.html");
+}
+function StaticMethods(){
+    location.href=("oop/oriented-StaticMethods.html");
+}
+function StaticProperties(){
+    location.href=("oop/oriented-StaticProperties.html");
+}
+function Namespaces(){
+    location.href=("oop/oriented-Namespaces.html");
+}
+function ObjectIteration(){
+    location.href=("oop/oriented-ObjectIteration.html");
+}
+function Encapsulation(){
+    location.href=("oop/oriented-Encapsulation.html");
+}
+function FinalKeyword(){
+    location.href=("oop/oriented-FinalKeyword.html");
+}
+function Overloading(){
+    location.href=("oop/oriented-Overloading.html");
+}
+function CloningObjects(){
+    location.href=("oop/oriented-CloningObjects.html");
+}
+function AnonymousClasses(){
+    location.href=("oop/oriented-AnonymousClasses.html");
+}
+
+
+/* WEB */
+function WebConcepts(){
+    location.href=("web/web-WebConcepts.html");
+}
+function FormHandling(){
+    location.href=("web/web-FormHandling.html");
+}
+function FormValidation(){
+    location.href=("web/web-FormValidation.html");
+}
+function formEmailURL(){
+    location.href=("web/web-formEmailURL.html");
+}
+
+function CompleteForm(){
+    location.href=("web/web-CompleteForm.html");
+}
+
+function FileInclusion(){
+    location.href=("web/web-FileInclusion.html");
+}
+
+function POST(){
+    location.href=("web/web-POST.html");
+}
+
+function FileUploading(){
+    location.href=("web/web-FileUploading.html");
+}
+
+function Cookies(){
+    location.href=("web/web-Cookies.html");
+}
+
+function Sessions(){
+    location.href=("web/web-Sessions.html");
+}
+
+function SessionOptions(){
+    location.href=("web/web-SessionOptions.html");
+}
+
+function SendingEmails(){
+    location.href=("web/web-SendingEmails.html");
+}
+
+function SanitizeInput(){
+    location.href=("web/web-SanitizeInput.html");
+}
+
+function PostRedirectGet(){
+    location.href=("web/web-PostRedirectGet.html");
+}
+
+function FlashMessages(){
+    location.href=("web/web-FlashMessages.html");
+}
+
+
+/* AJAX */
+function ajaxAutoComplete(){
+    location.href=("ajax/ajax-ajaxAutoComplete.html");
+}
+function ajaxIntroduction(){
+    location.href=("ajax/ajax-ajaxIntroduction.html");
+}
+
+function ajaxRSSFeed(){
+    location.href=("ajax/ajax-ajaxRSSFeed.html");
+}
+
+function ajaxSearch(){
+    location.href=("ajax/ajax-ajaxSearch.html");
+}
+
+function ajaxXMLParser(){
+    location.href=("ajax/ajax-ajaxXMLParser.html");
+}
+
+
+
+/* LOGIN */
+function LoginExample(){
+    location.href=("login/login-LoginExample.html");
+}
+function FacebookLogin(){
+    location.href=("login/login-FacebookLogin.html");
+}
+function PaypalIntegration(){
+    location.href=("login/login-PaypalIntegration.html");
+}
+function MySQLLogin(){
+    location.href=("login/login-MySQLLogin.html");
+}
+
+ /* USEFUL */
+
+ function QuestionAnswers(){
+    location.href=("useful/useful-QuestionAnswers.html");
+}
+function QuickGuide(){
+    location.href=("useful/useful-QuickGuide.html");
+}
+function UsefulResources(){
+    location.href=("useful/useful-UsefulResources.html");
+}
+function Discussion(){
+    location.href=("useful/useful-Discussion.html");
+}
+
+
+
+/* ADVANCE */
+function AMySQL(){
+    location.href=("advanced/advance-MySQL.html");
+}
+function INIFileConfiguration(){
+    location.href=("advanced/advance-INIFileConfiguration.html");
+}
+function ArrayDestructuring(){
+    location.href=("advanced/advance-ArrayDestructuring.html");
+}
+function CodingStandard(){
+    location.href=("advanced/advance-CodingStandard.html");
+}
+function RegularExpression(){
+    location.href=("advanced/advance-RegularExpression.html");
+}
+function ErrorHandling(){
+    location.href=("advanced/advance-ErrorHandling.html");
+}
+function TryCatch(){
+    location.href=("advanced/advance-Try..Catch.html");
+}
+function BugsDebugging(){
+    location.href=("advanced/advance-BugsDebugging.html");
+}
+function ForCDevelopers(){
+    location.href=("advanced/advance-ForCDevelopers.html");
+}
+function ForPERLDevelopers(){
+    location.href=("advanced/advance-ForPERLDevelopers.html");
+}
+function Frameworks(){
+    location.href=("advanced/advance-Frameworks.html");
+}
+function CorePHPvsFrameworks(){
+    location.href=("advanced/advance-CorePHPvsFrameworks.html");
+}
+function DesignPatterns(){
+    location.href=("advanced/advance-DesignPatterns.html");
+}
+function Filters(){
+    location.href=("advanced/advance-Filters.html");
+}
+function JSON(){
+    location.href=("advanced/advance-JSON.html");
+}
+function Exceptions(){
+    location.href=("advanced/advance-Exceptions.html");
+}
+function Special(){
+    location.href=("advanced/advance-SpecialTypes.html");
+}
+function Hashing(){
+    location.href=("advanced/advance-Hashing.html");
+}
+function Encryption(){
+    location.href=("advanced/advance-Encryption.html");
+}
+function inullFunction(){
+    location.href=("advanced/advance-i_nullFunction.html");
+}
+function Systemcalls(){
+    location.href=("advanced/advance-Systemcalls.html");
+}
+function HTTPAuthentication(){
+    location.href=("advanced/advance-HTTPAuthentication.html");
+}
+function SwappingVariables(){
+    location.href=("advanced/advance-SwappingVariables.html");
+}
+function Closure(){
+    location.href=("advanced/advance-Closure.html");
+}
+function Filteredunserialize(){
+    location.href=("advanced/advance-Filteredunserialize.html");
+}
+function Intlchar(){
+    location.href=("advanced/advance-Intlchar.html");
+}
+function CSPRNG(){
+    location.href=("advanced/advance-CSPRNG.html");
+}
+
+function Expectations(){
+    location.href=("advanced/advance-Expectations.html");
+}
+function UseStatement(){
+    location.href=("advanced/advance-UseStatement.html");
+}
+function IntegerDivision(){
+    location.href=("advanced/advance-IntegerDivision.html");
+}
+function DeprecatedFeatures(){
+    location.href=("advanced/advance-DeprecatedFeatures.html");
+}
+
+function RemovedExtensions(){
+    location.href=("advanced/advance-RemovedExtensions.html");
+}
+function PEAR(){
+    location.href=("advanced/advance-PEAR.html");
+}
+function CSRF(){
+    location.href=("advanced/advance-CSRF.html");
+}
+function FastCGiProcess(){
+    location.href=("advanced/advance-FastCGiProcess.html");
+}
+
+function PDOExtension(){
+    location.href=("advanced/advance-PDOExtension.html");
+}
+function BuiltInFunctions(){
+    location.href=("advanced/advance-BuiltFunctions.html");
+}
+
+
+/* XML */
+function xmlintroduction(){
+    location.href=("xml/xml-xmlintroduction.html");
+}
+function xmlparser(){
+    location.href=("xml/xml-xmlparser.html");
+}
+function saxParser(){
+    location.href=("xml/xml-saxParser.html");
+}
+function DOMParses(){
+    location.href=("xml/xml-DOMParses.html");
+}
+
+
+
